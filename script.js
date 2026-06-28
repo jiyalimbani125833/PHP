77835cbee0691ddec0a2d260f838d49fc1f96812
jiyalimbani125833 <jiya.limbani125833@marwadiuniversity.ac.in>
2026-06-28 12:25:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("loginForm").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if(username === "admin" && password === "1234"){
+        document.getElementById("message").innerHTML = "Login Successful!";
+    } else {
+        document.getElementById("message").innerHTML = "Invalid Username or Password!";
+    }
+});
